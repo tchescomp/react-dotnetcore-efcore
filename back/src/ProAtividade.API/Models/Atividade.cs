d44cf3ca520be9c140d73ee2e9870c07e525a864
Francisco Matos Pereira <tchescomatos@gmail.com>
2022-08-07 20:39:32 -0300

    ASP.NET 5 - Introdução

---
+++ back/src/ProAtividade.API/Models/Atividade.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAtividade.API.Models
+{
+    public class Atividade
+    {
+        public Atividade(){}
+
+        public Atividade(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Prioridade { get; set; }
+    }
+}
